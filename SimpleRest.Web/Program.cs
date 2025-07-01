8095aa594d222e7c33626fbc3564de8630d1330f
Colin C Cook <colinccook@gmail.com>
2025-07-01 07:27:18 +0100

    added health check

--- SimpleRest.Web/Program.cs
+++ SimpleRest.Web/Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHealthChecks();
+
 var app = builder.Build();
 
+app.MapHealthChecks("/health");
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
